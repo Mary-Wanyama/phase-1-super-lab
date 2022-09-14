91141f4f6350662f7e399c6fc757b4c937582b9f
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-14 04:29:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your classes here
+class tree {
+    constructor(species, definition){
+        this.species = species,
+        this.definittion = definittion
+        static definition(){
+            return definition
+        }
+    }
+}
